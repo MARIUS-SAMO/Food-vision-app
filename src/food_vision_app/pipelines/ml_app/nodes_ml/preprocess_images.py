23ec0ecc76515fc1b4c951dbf2cb70e2f452df4b
MARIUS-SAMO <samemarius@gmail.com>
2023-06-03 22:48:30 +0200

    ✨ Add preprocessing function

---
+++ src/food_vision_app/pipelines/ml_app/nodes_ml/preprocess_images.py
@@ -0,0 +1,42 @@
+from torchvision import transforms
+from typing import Tuple
+
+
+def create_transform_img_train(size: Tuple[int, int]) -> transforms.Compose:
+    """
+    Creates a data transformation pipeline for training images.
+
+    Args:
+        size (Tuple[int, int]): A tuple specifying the desired size of the transformed images.
+
+    Returns:
+        data_transform: A torchvision.transforms.Compose object representing the transformation pipeline.
+    """
+    data_transform = transforms.Compose([
+        transforms.Resize(size=size),
+        transforms.TrivialAugmentWide(),
+        transforms.ToTensor()
+    ])
+
+    return data_transform
+
+
+def create_transform_img_test(size: Tuple[int, int]) -> transforms.Compose:
+    """
+    Creates a data transformation pipeline for the testing images 
+
+    Args:
+        size (Tuple[int, int]): A tuple specifying the desired size of the transformed images.
+
+    Returns:
+        data_transform: A torchvision.transforms.Compose object representing the transformation pipeline.
+    """
+
+    data_transform = transforms.Compose(
+        [
+            transforms.Resize(size=size),
+            transforms.ToTensor()
+        ]
+    )
+
+    return data_transform
