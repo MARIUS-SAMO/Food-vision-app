a0b12cc9185a4de584e1f2f8271e5ccde875b803
MARIUS-SAMO <samemarius@gmail.com>
2023-06-27 23:48:09 +0200

    ✨ add etl pipeline

---
+++ src/food_vision_app/pipelines/etl_app/pipeline.py
@@ -0,0 +1,15 @@
+from kedro.pipeline import node, Pipeline
+from .nodes.create_data import get_data
+
+
+def create_etl_pipeline(**kwargs):
+    pipeline_etl = Pipeline(
+        [
+            node(
+                func=get_data,
+                inputs=["data_url", "storage_path"],
+                outputs=None
+            )
+        ]
+    )
+    return pipeline_etl
