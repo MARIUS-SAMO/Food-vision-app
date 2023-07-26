ec2b9ce806052bd0a2ac6467a37ca05090686e37
MARIUS-SAMO <samemarius@gmail.com>
2023-07-26 23:49:00 +0200

    add pipeline registry

--- src/food_vision_app/pipeline_registry.py
+++ src/food_vision_app/pipeline_registry.py
@@ -4,6 +4,9 @@ from typing import Dict
 from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
 
+from food_vision_app.pipelines.etl_app.pipeline import create_etl_pipeline
+from food_vision_app.pipelines.ml_app.pipeline import create_ml_pipeline
+
 
 def register_pipelines() -> Dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -11,6 +14,12 @@ def register_pipelines() -> Dict[str, Pipeline]:
     Returns:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
-    pipelines = find_pipelines()
-    pipelines["__default__"] = sum(pipelines.values())
-    return pipelines
+
+    etl_pipeline = create_etl_pipeline()
+    ml_pipeline = create_ml_pipeline()
+
+    return {
+        "etl_pipeline": etl_pipeline,
+        "ml_pipeline": ml_pipeline,
+        "__default__": etl_pipeline + ml_pipeline
+    }
